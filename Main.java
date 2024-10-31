bdfed3f2d27685d215e473abc9f493f98f96989f
IgorSerkov <93199459+IgorSerkov@users.noreply.github.com>
2024-10-31 23:34:21 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,61 +1,37 @@
-import org.dstu.dao.ArithmeticDao;
-import org.dstu.dao.GeometricDao;
-import org.dstu.dao.ProgressionDao;
+package org.dstu;
+
+import org.dstu.domain.IMathematics;
 import org.dstu.domain.Arithmetic;
 import org.dstu.domain.Geometric;
-import org.dstu.domain.Progression;
-import org.dstu.util.HibernateUtil;
-import org.hibernate.Metamodel;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+import org.dstu.tools.MyCsvReader;
+import org.dstu.tools.Serializer;
 
-import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-
-    public static void main(final String[] args) throws Exception {
-        final Session session = HibernateUtil.getSession();
-        GeometricDao geometricDao = new geometricDao();
-
-        Geometric geometric = new  Geometric();
-
-        geometric.setName(" arifmetic 5463");
-        geometric.setQuantity("5463");
-        geometric.setType(356);
-        geometricDao.save(geometric);
-
-        ArithmeticDao arithmeticDao = new ArithmeticDao();
-
-        Arithmetic arithmetic = new arithmetic();
-
-        arithmetic.setName("arifmetic");
-        arithmetic.setType("geometric");
-        arithmetic.setQuantity(5678);
-        arithmeticDao.save(arithmetic);
-
-        GeometricDao GeometricDao = new GeometricDao();
-
-        Geometric geometric = new Geometric();
-        Geometric.setName("geo");
-        geometric.setQuantity(3456789);
-        geometric.setType("geomet");
-        geometricDao.save(geometric);
-
-
-
-        try {
-            System.out.println("querying all the managed entities...");
-            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-            for (EntityType<?> entityType : metamodel.getEntities()) {
-                final String entityName = entityType.getName();
-                final Query query = session.createQuery("from " + entityName);
-                System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list()) {
-                    System.out.println("  " + o);
-                }
-            }
-        } finally {
-            session.close();
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Need input and output file arguments!");
+            return;
         }
+
+        List<String[]> strings = MyCsvReader.readCsv(args[0], ";");
+        System.out.println(strings.size());
+        List<IMathematics> people = new ArrayList<>();
+        for (String[] strArray: strings) {
+            // Create teachers and students
+            if (strArray[0].equals("0")) {
+                people.add(new Geometric(strArray));
+            } else {
+                people.add(new Arithmetic(strArray));
+            }
+        }
+
+        people.forEach(IMathematics::printInfo);
+
+        Serializer.serialize(people, args[1]);
+        List<IMathematics> newPeople = (List<IMathematics>) Serializer.deserialize(args[1]);
+        newPeople.forEach(IMathematics::printInfo);
     }
 }
